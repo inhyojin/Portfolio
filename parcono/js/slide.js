371ab1711c3d7d20ea165cfc7a48f5af762f25b8
inhyojin <happyhj4810@gmail.com>
2022-07-25 19:04:12 +0900

    slide 수정

--- parcono/js/slide.js
+++ parcono/js/slide.js
@@ -1,18 +1,24 @@
 let img = document.querySelectorAll('#mainImg');
 let nextBtn = document.querySelector('#nextBtn');
-let prevBtn = document.querySelector('#prevBtn')
+let prevBtn = document.querySelector('#prevBtn');
 
 let currentImg = 0;
 nextBtn.addEventListener('click', () => {
     let nextImg = (currentImg+1) % 3;
     img[currentImg].style.display = 'none';
     img[nextImg].style.display = 'block';
-    currentImg = nextImg
-
-    prevBtn.addEventListener('click', () => {
-        let previmg = (currentImg-1) % 3
+    currentImg = nextImg;
+});
+prevBtn.addEventListener('click', () => {
+    let previmg = currentImg-1;
+    if (previmg == -1) {
+        previmg = 2;
         img[currentImg].style.display = 'none';
         img[previmg].style.display = 'block';
         currentImg = previmg
-    });
+    } else {
+        img[currentImg].style.display = 'none';
+        img[previmg].style.display = 'block';
+        currentImg = previmg
+    }
 });
