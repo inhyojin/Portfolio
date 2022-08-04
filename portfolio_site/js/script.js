9b272c181e5f49207338d2a804822dedc5294499
inhyojin <happyhj4810@gmail.com>
2022-08-04 14:44:54 +0900

    포트폴리오 사이트

---
+++ portfolio_site/js/script.js
@@ -0,0 +1,10 @@
+
+// 연락 모달창
+const modalBox = document.querySelector('.modal');
+const btn = document.querySelector('.close');
+
+btn.addEventListener('click', (e) => {
+    modalBox.style.display = 'none';
+});
+
+
